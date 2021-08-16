d26357275721d9d727d7bcd67aa94cdf201b80f2
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-16 15:59:08 +0900

    Chapter10 スタート

---
+++ JavaPractice/src/Exception/Exception01.java
@@ -0,0 +1,24 @@
+package Exception;
+
+public class Exception01 {
+	  public static void main(String[] args) {
+	        System.out.println("100 ÷ 0 は？");
+	        int result = 100 / 0;
+	        System.out.println("計算結果" + result);
+	        System.out.println("プログラム終了");
+	    }
+}
+
+
+//package exception;
+//
+//public class Exception01 {
+//    public static void main(String[] args) {
+//        System.out.println("100 ÷ 0 は？");
+//        // 0で割り算するので、例外が発生
+//        int result = 100 / 0;
+//        // 例外が発生した以降の処理は実行されない
+//        System.out.println("計算結果" + result);
+//        System.out.println("プログラム終了");
+//    }
+//}
