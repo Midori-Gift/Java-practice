ef85a710d88414aa8029b6bd61a981e8674280bb
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-16 11:42:25 +0900

    Chapter08修了

--- JavaPractice/src/Chapter08.java
+++ JavaPractice/src/Chapter08.java
@@ -1,4 +1,20 @@
 
 public class Chapter08 {
+//	public static void main(String[] args) {
+//		int number01 = 1;
+//		while(number01 <= 5 ){
+//			number01*=number01;
+//			System.out.println(number01);
+//		}
+//	}
 
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4};
+
+		for (int number02 : array) {
+			System.out.println("For =" + number02);
+		}
+	}
 }
+
+
