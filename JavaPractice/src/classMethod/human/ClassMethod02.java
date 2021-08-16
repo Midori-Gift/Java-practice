66c346e16eaa5786aaee5df62d484a0435db9800
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-16 15:44:34 +0900

    Chapter9 修了

---
+++ JavaPractice/src/classMethod/human/ClassMethod02.java
@@ -0,0 +1,16 @@
+package classMethod.human;
+
+public class ClassMethod02 {
+    public String name;
+    public int age;
+
+    public ClassMethod02() {
+        name = "山田";
+        age = 20;
+    }
+
+    public ClassMethod02(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
