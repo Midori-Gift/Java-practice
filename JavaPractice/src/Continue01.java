87b4dd1f5cc9db49da3461eb389b3b889784e334
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-17 09:03:20 +0900

    chapter15 実力不足と判断し中断

--- JavaPractice/src/Continue01.java
+++ JavaPractice/src/Continue01.java
@@ -2,7 +2,7 @@
 public class Continue01 {
     public static void main(String[] args) {
         for (int count = 0; count < 5; count++) {
-            if (count == 2) {
+            if (count % 2 == 0) {
                 // countが2の場合、この後の処理をスキップ
                 continue;
             }
--- JavaPractice/src/practice/Chapter15_1.java
+++ JavaPractice/src/practice/Chapter15_1.java
@@ -1,5 +1,13 @@
 package practice;
 
 public class Chapter15_1 {
+	 int a = 976;
+     int b = 427;
+     System.out.println(a + " と " + b + " の最大公約数は、" + gcd(a, b) + "です。");
+}
 
+
+	static int gcd(int a, int b) {	
+
+	}
 }
